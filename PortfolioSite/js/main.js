8c5d167f5167507f81cb6883bc4d75fae3c73ce2
John Hiner <jrwenceslaus@gmail.com>
2017-04-12 18:26:50 -0400

    coming along

--- PortfolioSite/js/main.js
+++ PortfolioSite/js/main.js
@@ -1,4 +1,32 @@
+
+function zoomOut(){
+	$(this).off('click');
+	$(this).removeClass("picZoom");
+	$(this).click(zoomIn);
+}
+
+function zoomIn(){
+	$(this).off('click');
+	$(this).addClass("picZoom");
+	$(this).click(zoomOut);
+}
+
 $(document).ready(function(){
+	var waypoints = [];
+	var elements = document.getElementsByClassName('subheading');
+	
+	for(let i = 0; i < elements.length; i++){
+		waypoints.push(new Waypoint({
+																element: elements[i],
+																handler: function(){
+																	console.log(this.element);
+																	$(this.element).addClass("animated slideInLeft");
+																},
+																offset: '50%'}));
+	}
+	
+	$('.pic').click(zoomIn);
+	
 	$('a[href^="#"]').on('click',function (e) {
 	    e.preventDefault();
 
